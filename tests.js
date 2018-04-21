c1caa3608021e5354bc72a73ae3b4258254a90ed
Caitlin Lauer <caitlinrinn@gmail.com>
2018-04-21 19:54:07 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -4,7 +4,6 @@ var index = require("./index.js")
 if (index.name === "Joe") {
     console.log("The name is correct")
 
-var index = require("./index.js")
 }
 if (index.height === 74) {
     console.log("The height is correct")
